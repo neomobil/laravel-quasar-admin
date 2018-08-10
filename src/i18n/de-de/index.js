395097ff3c193658e3057eab29b7af15ab930752
kornel <takokornel@gmail.com>
2018-08-11 00:45:04 +0200

    translation update

--- src/i18n/de-de/index.js
+++ src/i18n/de-de/index.js
@@ -2,6 +2,9 @@
 // so you can safely delete all default props below
 
 export default {
+  appName: 'Quaravel',
   failed: 'Action failed',
+  home: 'Home',
+  poweredBy: 'Powered by',
   success: 'Action was successful'
 }
